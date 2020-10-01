17601bd6f8948dd8b8d3342318519d2049d6cd88
William <wwantoine@gmail.com>
2020-10-01 14:28:31 +0700

    update

--- src/components/box/index.js
+++ src/components/box/index.js
@@ -6,23 +6,25 @@ const Box = (id) => {
   const count = useSelector((state) => state.count);
   const color = useSelector((state) => state.color);
   const singleColor = useSelector((state) => state.singleColor);
-  const changeSingleColor = (event) => {
-    dispatch({
-      type: "CHANGE_SINGLE_COLOR",
-      payload: event.target.value,
-    });
-  };
+  // const changeSingleColor = (event) => {
+  //   dispatch({
+  //     type: "CHANGE_SINGLE_COLOR",
+  //     payload: event.target.value,
+  //   });
+  // };
   return (
     <div
       style={{
         width: "300px",
         height: "200px",
         border: "2px solid black",
-        backgroundColor: singleColor[index] || color,
+        backgroundColor: color,
       }}
     >
       <h1>This is a box</h1>
-      <input onChange={(event) => changeSingleColor(event)}></input>
+      <input
+      // onChange={(event) => changeSingleColor(event)}
+      ></input>
     </div>
   );
 };
